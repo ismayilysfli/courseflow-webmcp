fc6e5f2639317a2c97eaa38bef5391579e179a98
Ismayil Yusifli <ismayilyusifli90@gmail.com>
2026-08-30 13:25:17 +0400

    chore: update server port to use environment variable

--- server.ts
+++ server.ts
@@ -34,7 +34,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = 3000;
+const port = Number(process.env.PORT) || 3000;
 
 app.use(
   cors({
